2cdbeb69af96a65d283635c231f0587bb3120093
Akash-Kumar-Prasad <iakashprasad@gmail.com>
2024-04-18 20:48:04 +0530

    blade, route, controller basics done

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\MyController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('test', function(){
+    return view('home');
+});
+
+Route::view('homepage', 'home');
+
+Route::get('pdata',[MyController::class,'setdata']);
