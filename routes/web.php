a9c6640304aff9d4eecce02f354646bc5482c470
Akash-Kumar-Prasad <iakashprasad@gmail.com>
2024-04-20 16:51:52 +0530

    routing

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,55 @@ Route::get('test', function(){
     return view('home');
 });
 
-Route::view('homepage', 'home');
+// Route::view('homepage', 'home');
 
-Route::get('pdata', [MyController::class,'setdata']);
+// Route::get('pdata', [MyController::class,'setdata']);
+
+// Route::get('/user/{age}', function($age){
+// if($age<30)
+//         echo "Welcome to portal";
+// else
+//     echo "Not welcome";
+// });
+
+// Route::get('/first/{fname}/middle/{mname}', function($fname, $mname){
+// echo "hello $fname $mname";
+// });
+
+// Route::get('/{n1}/{n2}', function($n1, $n2){
+//  echo $n1+$n2;
+// });
+
+// Route::get('/{action}/{n1}/{n2}', function($action,$n1,$n2){
+
+//     if($action=='add')
+//         echo "Addition " .$n1+$n2;
+//     if($action =='sub')
+//         echo "Subtraction " .$n1-$n2;
+//     if($action == 'mul')
+//         echo "Multiplication ".$n1*$n2;
+//     if($action =='div')
+//         echo "Division ".$n1/$n2;
+
+// });
+
+// Route::get('/user/{name?}', function($name="anonymous"){
+// echo "hello"." ".$name;
+// });
+
+// Route::get('/user/{name}',function($name){
+// echo $name;
+// })->where('name', '[A-Za-z]+');
+
+Route::get('/emp/{id}',function($id){
+    echo "Employee ID ".$id;
+})->where('id','[0-9]+');
+
+Route::get('/name/{name}/age/{age}', function($name, $age){
+    echo "Name : ".$name. " Age : ".$age;
+})->where(['name'=>'[A-Za-z]+', 'age'=>'[0-9]+']);
+
+
+Route::fallback(function(){
+    echo "this url is not allowed in this URL";
+});
